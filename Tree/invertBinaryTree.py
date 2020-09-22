d7ee96cae7974cc3fa403159dbea3a1002e9637e
anugrah18 <anugrah18@gmail>
2020-09-21 22:00:52 -0700

    Upload via bat script

---
+++ Tree/invertBinaryTree.py
@@ -0,0 +1,32 @@
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def invertTree(self, root):
+        if not root:
+            return None
+
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+
+        root.left = right
+        root.right = left
+
+        return root
+
+
+tree= TreeNode(4)
+tree.left = TreeNode(2)
+tree.right = TreeNode(7)
+tree.left.left = TreeNode(1)
+tree.left.right = TreeNode(3)
+tree.right.left = TreeNode(6)
+tree.right.right = TreeNode(9)
+
+X =Solution()
+invertTree= X.invertTree(tree)
+

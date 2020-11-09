9b433430e552f4aa32cb4349fcf42bdf9f37b5c9
anugrah18 <anugrah18@gmail>
2020-11-09 00:34:58 -0800

    Upload via bat script

---
+++ Arrays/rankTeamByVotes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rankTeam(self, votes):
+        x = len(votes[0])
+        rank = {}
+
+        for word in votes:
+            for i,char in enumerate(word):
+                if char not in rank:
+                    rank[char] = [0]*x
+                rank[char][i] = rank[char][i]+1
+
+        rank = list(rank.items())
+        rank.sort(reverse=True, key=lambda x: (x[1], [ord(x[0])]))
+
+        rank = [i[0] for i in rank]
+        return ''.join(rank)
+
+
+X = Solution()
+print(X.rankTeam(["ABC","ACB","ABC","ACB","ACB"]))
+
+

f941757c6fd3ea95bc44d350b2359b197db0f35e
anugrah18 <anugrah18@gmail>
2021-03-07 22:05:44 -0800

    Upload via bat script

---
+++ Arrays&Strings/minMovesToEqualArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMoves(self, nums) -> int:
+        nums = sorted(nums)
+        count = 0
+        for i in range(len(nums) - 1, 0, -1):
+            count += nums[i] - nums[0]
+
+        return count
+
+X =Solution()
+print(X.minMoves([1,2,3]))
+
---
+++ Hash table/dotProductOfSparseVectors.py
@@ -0,0 +1,21 @@
+class SparseVector:
+    def __init__(self, nums):
+        self.nonzeros = {}
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.nonzeros[i] = n
+
+
+
+    def dotProduct(self, vec) -> int:
+        result = 0
+
+        for i, n in self.nonzeros.items():
+            if i in vec.nonzeros:
+                result += n * vec.nonzeros[i]
+
+        return result
+
+V1 = SparseVector([1,0,0,2,3])
+V2 = SparseVector([0,3,0,4,0])
+print(V1.dotProduct(V2))

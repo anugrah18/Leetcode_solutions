2a2370f5281053bf3270cf081729cfc90b5bcad5
anugrah18 <anugrah18@gmail>
2022-04-19 21:07:50 -0700

    Upload via bat script

--- Backtracking/39-combinationSum.py
+++ Backtracking/39-combinationSum.py
@@ -1,5 +1,24 @@
 class Solution:
-    def combinationSum(self, candidates, target):
+    def combinationSum_I(self, candidates, target):
+        res = []
+
+        def dfs(i, curr, total):
+            if total == target:
+                # shallow copy of curr
+                res.append(curr.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            curr.append(candidates[i])
+            dfs(i, curr, total + candidates[i])
+            curr.pop()
+            dfs(i + 1, curr, total)
+
+        dfs(0, [], 0)
+        return res
+
+    def combinationSum_II(self, candidates, target):
 
         result = []
 
@@ -19,4 +38,5 @@ class Solution:
         return result
 
 X = Solution()
-print(X.combinationSum([2,3,6,7],7))
+print(X.combinationSum_I([2,3,6,7],7))
+print(X.combinationSum_II([2,3,6,7],7))

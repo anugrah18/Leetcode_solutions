69855e50584e0cb88e2753e9790fb6f461b65059
anugrah18 <anugrah18@gmail>
2022-04-30 23:02:21 -0700

    Upload via bat script

--- Greedy/1647-minimumDeletionsToMakeCharFrequencyUnique.py
+++ Greedy/1647-minimumDeletionsToMakeCharFrequencyUnique.py
@@ -1,5 +1,25 @@
 class Solution:
     def minDeletion_I(self,s):
+        # Store the frequency of each character
+        frequency = [0] * 26
+        for char in s:
+            frequency[ord(char) - ord('a')] += 1
+
+        delete_count = 0
+        # Use a set to store the frequencies we have already seen
+        seen_frequencies = set()
+        for i in range(26):
+            # Keep decrementing the frequency until it is unique
+            while frequency[i] and frequency[i] in seen_frequencies:
+                frequency[i] -= 1
+                delete_count += 1
+
+            # Add the newly occupied frequency to the set
+            seen_frequencies.add(frequency[i])
+
+        return delete_count
+
+    def minDeletion_II(self,s):
         freq=[0]*26
         count=0
 
@@ -21,25 +41,7 @@ class Solution:
 
         return count
 
-    def minDeletion_II(self,s):
-        # Store the frequency of each character
-        frequency = [0] * 26
-        for char in s:
-            frequency[ord(char) - ord('a')] += 1
 
-        delete_count = 0
-        # Use a set to store the frequencies we have already seen
-        seen_frequencies = set()
-        for i in range(26):
-            # Keep decrementing the frequency until it is unique
-            while frequency[i] and frequency[i] in seen_frequencies:
-                frequency[i] -= 1
-                delete_count += 1
-
-            # Add the newly occupied frequency to the set
-            seen_frequencies.add(frequency[i])
-
-        return delete_count
 
 X = Solution()
 print(X.minDeletion_I('aaabbbccc'))
